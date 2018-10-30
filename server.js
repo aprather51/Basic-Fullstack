287ba4030fc171990f84108eaaa500cedc740055
aprather51 <apratherjr@gmail.com>
2018-10-30 14:55:58 -0500

    initial commit - added basic fullstack setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+// const multer = require('multer');
+// const path = require('path');
+
+//initate server
+const server = express();
+const port = process.env.PORT || 5000;
+
+//body-parser
+// server.use(bodyParser.urlencoded({ extended: true }));
+// server.use(bodyParser.json());
+
+server.get('/users', (req, res) => {
+	// res.send('Hello');
+	const users = [
+		{ id: 1, firstname: 'John', lastname: 'Doe' },
+		{ id: 2, firstname: 'Jane', lastname: 'Doe' },
+		{ id: 3, firstname: 'Jack', lastname: 'Doe' },
+		{ id: 4, firstname: 'Jill', lastname: 'Doe' }
+	];
+
+	res.json(users);
+});
+
+server.listen(port, () =>
+	console.log(`The server is listening on port ${port}`)
+);
